511b4b0bf95023415169c9ee752c5f64ecc87833
airidabu <airida.buinickaite@gmail.com>
2025-03-11 20:04:47 +0200

    added GenresPage

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Route, Routes} from "react-router";
 import Layout from "./components/Layout.tsx";
 import AuthorsPage from "./pages/AuthorsPage.tsx";
 import AuthorPage from "./pages/AuthorPage.tsx";
+import GenresPage from "./pages/GenresPage.tsx";
 
 const App: React.FC = () => {
 
@@ -14,6 +15,7 @@ const App: React.FC = () => {
             <Route index element={<HomePage/>}/>
             <Route path="/authors" element={<AuthorsPage/>}/>
             <Route path="/authors/:id" element={<AuthorPage />}/>
+            <Route path="/genres" element={<GenresPage/>}/>
             <Route path="/books/:id" element={<h1 />}/>
           </Route>
         </Routes>
