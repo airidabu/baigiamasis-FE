e0e5b4f5196576c35c2a2e5fe895bd46fe977095
airidabu <airida.buinickaite@gmail.com>
2025-03-14 14:03:17 +0200

    books context

---
+++ src/contexts/BooksContext.tsx
@@ -0,0 +1,59 @@
+import {BookAction, bookReducer, BookState, initialState} from "../reducers/bookReducer.ts";
+import {getBooks, addBook, deleteBook} from "../api/books.ts";
+import Book from "../types/Book.ts";
+import {createContext, Dispatch, ReactNode, useContext, useReducer} from "react";
+
+type BooksContextType = {
+    state: BookState;
+    dispatch: Dispatch<BookAction>;
+    fetchBooks: () => Promise<void>;
+    createBook: (book: Omit<Book, "id">) => Promise<void>;
+    removeBook: (id:string) => Promise<void>;
+}
+
+const BooksContext = createContext<BooksContextType | undefined>(undefined);
+
+export const BooksProvider: React.FC<{children: ReactNode}> = ({ children }) => {
+    const [state, dispatch] = useReducer(bookReducer, initialState);
+
+    const fetchBooks = async () => {
+        try {
+            const books = await getBooks();
+            dispatch({type: "GET_BOOKS", payload: books});
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    const createBook = async (book: Omit<Book, "id">) =>{
+        try {
+            const newBook = await addBook(book)
+            dispatch({type: "ADD_BOOK", payload: newBook})
+        }  catch (error) {
+            console.error(error);
+        }
+    }
+
+    const removeBook = async (bookId: string) => {
+        try {
+            deleteBook(bookId);
+            dispatch({type: "DELETE_BOOK", payload: bookId})
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    return (
+        <BooksContext.Provider value={{state, dispatch, fetchBooks, createBook, removeBook}}>
+            {children}
+        </BooksContext.Provider>
+    )
+}
+
+export const useBooks = () => {
+    const ctx = useContext(BooksContext);
+    if (!ctx) {
+        throw new Error("useBooks must be used within context.");
+    }
+    return ctx;
+}
