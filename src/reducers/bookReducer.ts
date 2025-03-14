8e703fb22a5a069fcecc5ce923e94e350400cb19
airidabu <airida.buinickaite@gmail.com>
2025-03-14 14:00:02 +0200

    book reducer

---
+++ src/reducers/bookReducer.ts
@@ -0,0 +1,29 @@
+import Book from "../types/Book.ts";
+
+export type BookState = {
+    books: Book[];
+    error: string|null;
+}
+
+export type BookAction =
+    | {type: "GET_BOOKS", payload: Book[]}
+    | {type: "ADD_BOOK", payload: Book}
+    | {type: "DELETE_BOOK", payload: string}
+
+export const initialState: BookState = {
+    books: [],
+    error: null
+};
+
+export const bookReducer = (state: BookState, action: BookAction) => {
+    switch (action.type) {
+        case "GET_BOOKS":
+            return {...state, books: action.payload};
+        case "ADD_BOOK":
+            return {...state, books: [...state.books, action.payload]};
+        case "DELETE_BOOK":
+            return {...state, books: state.books.filter(book => book.id !== action.payload) };
+        default:
+            return state;
+    }
+}
