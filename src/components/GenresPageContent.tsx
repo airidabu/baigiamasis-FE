0610b1b96a037c705d4f555342032f85e16d5248
airidabu <airida.buinickaite@gmail.com>
2025-03-15 03:15:15 +0200

    added styling to genres page

--- src/components/GenresPageContent.tsx
+++ src/components/GenresPageContent.tsx
@@ -2,6 +2,10 @@ import GenresForm from "./forms/GenresForm.tsx";
 import {useGenres} from "../contexts/GenresContext.tsx";
 import {useEffect} from "react";
 import ItemsList from "./ItemsList.tsx";
+import Box from "@mui/material/Box";
+import {Divider, ListItem, ListItemText} from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
 const GenresPageContent: React.FC = () => {
     const {state, fetchGenres, removeGenre} = useGenres();
@@ -11,18 +15,37 @@ const GenresPageContent: React.FC = () => {
     }, []);
 
     const createGenreElements = state.genres.map((genre) => (
-        <li key={genre.id}>{genre.name}
-            <button onClick={() => removeGenre(genre.id!)}>❌</button>
-        </li>
+        <ListItem
+            key={genre.id}
+            disableGutters
+            secondaryAction={
+                <IconButton onClick={() => removeGenre(genre.id!)} aria-label="delete">
+                    <DeleteForeverIcon/>
+                </IconButton>
+            }
+        >
+            <ListItemText primary={genre.name}/>
+        </ListItem>
     ))
 
     return (
-        <div>
+        <Box
+            sx={{
+                display: "flex",
+                flexDirection:{ xs: "column", md:"row"},
+                justifyContent: {md: "space-around", xs: "center"},
+                gap: 2,
+                maxWidth: "800px",
+                margin: "auto",
+                alignItems: "center",
+            }}
+        >
             <ItemsList children={createGenreElements}></ItemsList>
+            <Divider orientation="vertical" variant="middle" flexItem/>
             <div>
                 <GenresForm/>
             </div>
-        </div>
+        </Box>
     )
 }
 
