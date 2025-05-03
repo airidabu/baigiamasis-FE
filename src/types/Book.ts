d3a1aefa69c6adf42247cd51bcbd51ae15b409d8
airidabu <airida.buinickaite@gmail.com>
2025-05-03 22:14:21 +0300

    Refactor Book, Genre, Publisher, Review, and User type definitions for consistency and clarity

--- src/types/Book.ts
+++ src/types/Book.ts
@@ -4,18 +4,18 @@ import Publisher from "./Publisher";
 import BookStatus from "./BookStatus";
 
 interface Book {
-    _id?: string;
+    _id: string;
     name: string;
-    author: Author;
-    publisher: Publisher;
-    genres: Genre[];
-    pageNumber: number;
-    status: BookStatus;
+    author?: Author;
+    publisher?: Publisher;
+    genres?: Genre[];
+    pageNumber?: number;
+    status?: BookStatus;
     pictureUrl?: string;
-    releaseDate: string;
-    createdAt: string;
-    updatedAt: string;
-    __v: number;
+    releaseDate?: string;
+    createdAt?: string;
+    updatedAt?: string;
+    __v?: number;
     description?: string;
 }
 
