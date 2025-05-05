2761c2ffa1a98126abe4eea58c07af676ad2b9e8
airidabu <airida.buinickaite@gmail.com>
2025-05-05 15:12:34 +0300

    Add RegistrationForm and UnauthorizedPage components with routing

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,9 @@ import GenresPage from "./pages/GenresPage.tsx";
 import BooksPage from "./pages/BooksPage.tsx";
 import BookPage from "./pages/BookPage.tsx";
 import ReviewsPage from "./pages/ReviewsPage.tsx";
+import LoginForm from "./components/forms/LoginForm.tsx";
+import RegistrationForm from "./components/forms/RegistrationForm.tsx";
+import UnauthorizedPage from "./pages/UnauthorizedPage.tsx";
 
 const App: React.FC = () => {
 
@@ -18,10 +21,13 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/register" element={<RegistrationForm />} />
           <Route path="/genres" element={<GenresPage />} />
           <Route path="/books" element={<BooksPage />} />
           <Route path="/books/:id" element={<BookPage />} />
           <Route path="/reviews" element={<ReviewsPage />} />
+          <Route path="/unauthorized" element={<UnauthorizedPage />} />
         </Route>
       </Routes>
     </Router>
